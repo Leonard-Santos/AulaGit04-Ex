6ccf68d3f9095e34a0046c432fa63c8622b2c624
Leonardo Santos <leonardoeliano@gmail.com>
2021-09-19 19:19:52 -0300

    Edited calculator.js

---
+++ calculator.test.js
@@ -0,0 +1,15 @@
+// import { expect, test } from '@jest/globals';
+// import * as calculator from './calculator.js';
+const calculator = require('./calculator');
+
+test("testando a soma de 1+1", () => {
+    expect(calculator.soma(1,1)).toBe(2);
+})
+
+test("testando divisão de 42/6", () => {
+    expect(calculator.divisao(42,6)).toBe(7);
+})
+
+test("testando a soma de 5+5", () => {
+    expect(calculator.soma(5,5)).toBe(10);
+})
